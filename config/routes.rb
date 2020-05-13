7dc04f4cd88e7d6a81936af4771b24f944122ec8
vicPL1997 <victorperera85@gmail.com>
2020-05-13 21:48:20 +0100

    buscador

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :tienda
   resources :contacto
   resources :tutorial
+  get '/search' => 'pages#search', :as => 'search_page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
